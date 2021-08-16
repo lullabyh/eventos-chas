dc2b472a72658cac96be0a57350ae31e6a6fe24c
marianasilva <marianasilva@unimedce.com.br>
2021-08-16 09:56:52 -0300

    adiciona procfile

--- src/main/java/com/organiza/eventoschas/controller/Checklist.java
+++ src/main/java/com/organiza/eventoschas/controller/Checklist.java
@@ -1,10 +1,12 @@
 package com.organiza.eventoschas.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
+@RequestMapping
 public class Checklist {
 	
 	@GetMapping
